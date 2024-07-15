3c918d6b77346a34597707c6d8aec993dd4fcabf
Shayan Ajmal <shykizzy@Shayans-MacBook-Pro.local>
2024-07-15 20:41:43 +0500

    merge sorted lists complete

---
+++ Grind75/Week1/MergeSortedLists.cpp
@@ -0,0 +1,39 @@
+// watched https://www.youtube.com/watch?v=R9PTBwOzceo to gain basic understanding of linkedLists
+
+
+// intuition: compare heads of each list.. smaller becomes the head of the output list. then increment list with smaller elemnt and compare to other list.. always place smaller element into list and increment other until one list runs out then place entire other list into output as it is already sorted
+
+// Definition for singly-linked list.
+  struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy; 
+        ListNode *outputList = &dummy;
+
+        while(list1 !=nullptr && list2 != nullptr){
+            if(list1->val > list2->val){
+                outputList->next = list2; 
+                list2 = list2->next;
+            }else{
+                outputList->next = list1; 
+                list1 = list1->next;
+            }
+            outputList = outputList->next; // this basically insures that future vales get stored in the next element portion of the output list and continue to do so for each node, without this the loop is either infinite or you would continue to overwrite the first node in outputList
+        }
+        if(list1== nullptr){
+            outputList->next = list2;
+        }else{
+            outputList->next = list1;
+        }
+
+        return dummy.next; // since dummy was anarbitrary header we used to initialize our linked list we want to return its next element as that is what contianes the merged list
+    }
+};
--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 This repository contains my iterative appraoch to learning data structures and algorithms. Here I will include my solutions to common coding exercise packages, these solutions will include my thought process, approach and solution. The goal of this is not only to develop an intutive understanding of data structures and algorithms but also to become comfortable programming in c++. 
 
-## Additional Notes
-
-I will include some solutions containing the key word refined in the name of the file as these solutions will be new approaches to the problem after having taking a data structures and algorithms course. These solutions will contain an explanation as to why the proposed solution is better.
-
 ## Link to Schedule
 
 <https://www.techinterviewhandbook.org/grind75?weeks=8&hours=8>
